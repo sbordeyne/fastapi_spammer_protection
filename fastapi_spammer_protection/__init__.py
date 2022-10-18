c19b768f3e847744fa46e44918b608dafa0ab330
Dogeek <simon.bordeyne@gmail.com>
2022-10-18 08:13:06 +0200

    [mod] add middleware to __init__.py

--- fastapi_spammer_protection/__init__.py
+++ fastapi_spammer_protection/__init__.py
@@ -1,4 +1,6 @@
 '''
 Middleware to protect a FastAPI app against spammers that try to exploit known vulnerabilities
 '''
-__version__ = '1.0.0'
+__version__ = '1.0.1'
+
+from .middleware import SpammerProtection  # noqa
