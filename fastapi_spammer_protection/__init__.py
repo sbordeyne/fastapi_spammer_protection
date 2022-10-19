812b140ce1dce97b49ee54be4dac65b22c37475a
Dogeek <simon.bordeyne@gmail.com>
2022-10-19 07:08:56 +0200

    [mod] version bump to 1.1.0

--- fastapi_spammer_protection/__init__.py
+++ fastapi_spammer_protection/__init__.py
@@ -1,6 +1,6 @@
 '''
 Middleware to protect a FastAPI app against spammers that try to exploit known vulnerabilities
 '''
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 from .middleware import SpammerProtection  # noqa
